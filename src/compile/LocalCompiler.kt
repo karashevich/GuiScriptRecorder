915aaa49bdb205ba3c020762a5cb6edcf57dcc98
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2016-12-29 17:23:25 +0300

    maven plugin dependency

--- src/compile/LocalCompiler.kt
+++ src/compile/LocalCompiler.kt
@@ -1,28 +1,30 @@
 package compile
 
-//import org.jetbrains.kotlin.maven.K2JVMCompileMojo
+import com.intellij.openapi.util.io.FileUtil
+import org.jetbrains.kotlin.maven.K2JVMCompileMojo
+import java.io.File
 
 /**
  * @author Sergey Karashevich
  */
 class LocalCompiler {
 
-//    val helloKtText = "fun main(args: Array<String>) { \n println(\"Hello, World!\") \n }"
-//
-//    inner class MyCompiler : K2JVMCompileMojo() {
-//    }
-//
-//    val myCompiler by lazy {
-//        MyCompiler().execute()
-//    }
-//
-//    fun createTempFile(code: String) : File {
-//        val tempFile = FileUtil.createTempFile("CurrentTest", ".kt", true)
-//        FileUtil.writeToFile(tempFile, code, false)
-//        return tempFile
-//    }
-//
-//    fun compile(fileKt: File, classpath: String){
-//
-//    }
+    val helloKtText = "fun main(args: Array<String>) { \n println(\"Hello, World!\") \n }"
+
+    inner class MyCompiler : K2JVMCompileMojo() {
+    }
+
+    val myCompiler by lazy {
+        MyCompiler().execute()
+    }
+
+    fun createTempFile(code: String) : File {
+        val tempFile = FileUtil.createTempFile("CurrentTest", ".kt", true)
+        FileUtil.writeToFile(tempFile, code, false)
+        return tempFile
+    }
+
+    fun compile(fileKt: File, classpath: String){
+
+    }
 }
