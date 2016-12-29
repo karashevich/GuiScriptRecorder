db0c6b9fd83e3e97b2adafa33c7c45428ec007f4
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2016-12-29 17:15:12 +0300

    updated GuiScriptRecorder.zip

---
+++ src/compile/LocalCompiler.kt
@@ -0,0 +1,28 @@
+package compile
+
+//import org.jetbrains.kotlin.maven.K2JVMCompileMojo
+
+/**
+ * @author Sergey Karashevich
+ */
+class LocalCompiler {
+
+//    val helloKtText = "fun main(args: Array<String>) { \n println(\"Hello, World!\") \n }"
+//
+//    inner class MyCompiler : K2JVMCompileMojo() {
+//    }
+//
+//    val myCompiler by lazy {
+//        MyCompiler().execute()
+//    }
+//
+//    fun createTempFile(code: String) : File {
+//        val tempFile = FileUtil.createTempFile("CurrentTest", ".kt", true)
+//        FileUtil.writeToFile(tempFile, code, false)
+//        return tempFile
+//    }
+//
+//    fun compile(fileKt: File, classpath: String){
+//
+//    }
+}
